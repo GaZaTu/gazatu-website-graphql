6fa124e12e6dbc4fc9dcbd0fac32f0dbfd386b1c
GaZaTu <dennis.ballmann@gmail.com>
2020-02-22 21:20:05 +0100

    add submitters param to /trivia/questions doc

--- src/views/StartView.tsx
+++ src/views/StartView.tsx
@@ -13,6 +13,7 @@ const StartView: React.FC = () => {
         <li><b>count</b> (number): amount of questions to return (does not affect shuffling)</li>
         <li><b>exclude</b> ([categoryName,...]): list of categories to exclude</li>
         <li><b>include</b> ([categoryName,...]): list of categories to include</li>
+        <li><b>submitters</b> ([submitterNames,...]): list of submitters to include</li>
       </ul>
       <p>Example: <a href={`${process.env.REACT_APP_API_URL}/trivia/questions?count=10&exclude=[Anime,Hentai]`} target="_blank" rel="noopener noreferrer">{`${process.env.REACT_APP_API_URL}/trivia/questions?count=10&exclude=[Anime,Hentai]`}</a></p>
     </div>
