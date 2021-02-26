9fada5912b022178901625c780fc48b231892b16
GaZaTu <dennis.ballmann@gmail.com>
2021-02-26 18:58:16 +0100

    fix service-worker for cra4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorkerRegistration'
 import App from './App'
 import { StoreProvider } from './store'
 import './index.css'
